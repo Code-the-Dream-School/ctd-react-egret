914b03be6d39169a34af15d9d031c249abf08d15
NGFuture <78330887+NGFuture@users.noreply.github.com>
2021-08-19 13:11:38 -0700

    lesson2

---
+++ less1.1/src/AddTodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddTodoForm = () => (
+    <form>
+        <label htmlFor='todoTitle'>Title </label>
+        <input id='todoTitle' />
+        <button>Add</button>
+    </form>
+);
+
+export default AddTodoForm;
--- less1.1/src/App.js
+++ less1.1/src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
-
-let todoList=[
-  {
-    id: 1,
-    title: "Go shopping",
-  }, 
-  {
-    id: 2,
-    title: "Relax",
-  },
-  {
-    id: 3,
-    title: "Cook dinner",
-  }
-];
+import TodoList from './TodoList.js';
+import AddTodoForm from './AddTodoForm.js';
 
 function App() {
   return (
     // <> called fragment, used instead of div not to create to many divs
     <> 
       <h1>TODO list</h1>
-      <ul>
-        {todoList.map(function(item) {
-          return <li key={item.id}>{item.title}</li>;
-        })}
-      </ul>
+      <AddTodoForm />
+      <TodoList />
     </>
   );
 }
