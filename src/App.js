4ec06ccf9995e489dec1b60efe51f1554c95f5ec
OmarAlareeki <omarproductions2018@gmail.com>
2021-08-21 09:18:21 -0700

    fixed last commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import TodoList from './TodoList'
-import AddTodoForm from './AddTodoForm'
 import './style.css'
+
+
+const todoList = [
+  {
+    'id': 1,
+    'title': 'Complete the coding challenge on uidaily.com'
+  },
+  {
+    'id': 2,
+    'title': 'Search for a remote job'
+  },
+  {
+    'id': 3,
+    'title': 'Ask for help moving furniture next month'
+  },
+]
 
 function App() {
   return (
@@ -9,10 +23,19 @@ function App() {
       <header>
         <h1>Todo List</h1>
       </header>
-        <AddTodoForm />
-        <TodoList />
+        <List />
     </div>
   );
 }
+
+const List = () => (
+  <ul>
+  {todoList.map((item) => (
+    <li key={item.id}>
+      <span>{item.title}</span>
+    </li>
+  ))}
+  </ul>
+)
 
 export default App;
