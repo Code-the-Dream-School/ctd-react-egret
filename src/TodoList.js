8dce6228d902ac512139c9b53dc33f907420c9d2
Alena <83471057+alenamedved@users.noreply.github.com>
2021-08-23 22:18:38 -0500

    changed to function renderList

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,32 +1,34 @@
 import React from 'react'
 
 const todoList = [
-    {
-      id: 1,
-      title: 'Clone the ctd-react-egret repo'
-    },
-    {
-      id: 2,
-      title: 'Create a new branch and name it lesson-1-1'
-    },
-    {
-      id: 3,
-      title: 'Complete the assignment'
-    },
-    {
-      id: 4,
-      title: 'Submit a pull request'
-    }
-  ]
+  {
+    id: 1,
+    title: 'Clone the ctd-react-egret repo'
+  },
+  {
+    id: 2,
+    title: 'Create a new branch and name it lesson-1-1'
+  },
+  {
+    id: 3,
+    title: 'Complete the assignment'
+  },
+  {
+    id: 4,
+    title: 'Submit a pull request'
+  }
+]
 
+const renderList = (list) => {
+  return list.map(item => <li key={item.id}>{item.title}</li>)
+}
 
 function TodoList() {
-    return (
-        <ul>
-        {todoList.map( item => <li key={item.id}>{item.title}</li>
-        )}
-        </ul>
-    )
+  return (
+    <ul>
+      {renderList(todoList)}
+    </ul>
+  )
 }
 
 export default TodoList
