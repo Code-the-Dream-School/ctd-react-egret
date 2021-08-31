d0f274c1fe3504cca298306d5b42e6e3fde43ee1
OmarAlareeki <omarproductions2018@gmail.com>
2021-08-30 18:17:59 -0700

    changes list items

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,19 +2,19 @@ import React from 'react';
 import TodoListItem from'./TodoListItem';
 
 const todoList = [
-    {
-      'id': 1,
-      'title': 'React'
-    },
-    {
-      'id': 2,
-      'title': 'Html'
-    },
-    {
-      'id': 3,
-      'title': 'Css'
-    },
-  ]
+  {
+    'id': 1,
+    'title': 'Complete the coding challenge on uidaily.com'
+  },
+  {
+    'id': 2,
+    'title': 'Search for a remote job'
+  },
+  {
+    'id': 3,
+    'title': 'Ask for help moving furniture next month'
+  },
+]
   
   const TodoList = (props) => {
       return (
