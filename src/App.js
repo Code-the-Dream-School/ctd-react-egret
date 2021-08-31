3757e99d6dd6adfe774687d84a8d0e743cbc73b2
Chung Kao <chungckao@gmail.com>
2021-09-01 01:04:28 +0800

    Complete lesson 1.3 assignment

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
-const App = () => (
-	<div>
-		<h1>Todo List</h1>
-		<AddTodoForm />
-		<TodoList />
-	</div>
-);
+const App = () => {
+	const [newTodo, setNewTodo] = React.useState("");
+
+	return (
+		<div>
+			<h1>Todo List</h1>
+			<AddTodoForm onAddTodo={setNewTodo} />
+			{newTodo ? <p>Successfully added: "{newTodo}"</p> : null}
+			<TodoList />
+		</div>
+	);
+};
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,14 @@
 import React from "react";
+import TodoListItem from "./TodoListItem";
 
 const todoList = [
 	{
 		id: 1,
-		title: "Complete assignment for lesson 1.1",
+		title: "Complete assignment for lesson 1.3",
 	},
 	{
 		id: 2,
-		title: "Commit and push to lesson-1-1 branch on GitHub",
+		title: "Commit and push to lesson-1-3 branch on GitHub",
 	},
 	{
 		id: 3,
@@ -22,7 +23,7 @@ const todoList = [
 const TodoList = () => (
 	<ul>
 		{todoList.map((item) => (
-			<li key={item.id}>{item.title}</li>
+			<TodoListItem key={item.id} item={item} />
 		))}
 	</ul>
 );
