df8bd2c8157e1365105973ec310c01c5a4333022
Ignat Babenko <ignatbabenko@Ignats-MacBook-Air.local>
2021-08-31 20:23:55 -0700

    lesson-1-3

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-function AddTodoForm() {
+function AddTodoForm(props) {
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+        const todoTitle = event.target.title.value;
+        console.log(todoTitle);
+        props.onAddTodo(todoTitle);
+        event.target.reset();
+    }
     return (
-        <form>
+        <form onSubmit={handleAddTodo}>
             <label htmlFor='todoTitle'>Title: </label>
-            <input id='todoTitle'></input> 
+            <input id='todoTitle' type='text' name='title'></input>
             <button type='submit'>Add</button>
         </form>
     );
