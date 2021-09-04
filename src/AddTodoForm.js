0b9614bca15d9dff3d55169cc3859e6508ba4538
OmarAlareeki <omarproductions2018@gmail.com>
2021-09-03 18:05:52 -0700

    made changes to check and learn

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,23 +1,24 @@
 import React from 'react'
 
 const AddTodoForm = (props) => {
+    
 
     const handleAddTodo = (event) => {
+
         event.preventDefault();
-        let todoTitle = event.target.title.value;
-        console.log(todoTitle);
-        console.log(event);
-        props.onAddTodo(todoTitle);
+        let titleTodo = event.target.title.value;
+        props.onAddTodo(titleTodo)
         event.target.reset();
+
     }
 
     return (
         <div>
-        <form onSubmit={handleAddTodo}>
-            <label htmlFor="todoTitle" >Title: </label>
-            <input id="todoTitle" type="text" name="title" />
-            <button type="submit">Add</button>
-        </form>
+            <form onSubmit={handleAddTodo}>
+                <label htmlFor="todoTitle" >Title: </label>
+                <input id="todoTitle" type="text" name="title" />
+                <button type="submit">Add</button>
+            </form>
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
-import './style.css'
+import './style.css';
+
 
 function App() {
-
-  const [newTodo, setNewTodo] = React.useState("")
+  
+  const [newTodo, setNewTodo] = React.useState("");
 
   return (
-    <div className="container">
+    <div>
       <header>
         <h1>Todo List</h1>
       </header>
         <AddTodoForm onAddTodo={setNewTodo}/>
-        <p>{newTodo}</p>
+        <p>{newTodo}</p>      
         <TodoList />
     </div>
   );
