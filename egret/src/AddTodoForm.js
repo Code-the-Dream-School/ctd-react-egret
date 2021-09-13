41b40aa3ec954f42c668c98f13b343ad25c57438
Kenneth <kenldurham@gmail.com>
2021-09-13 15:20:17 -0700

    1-3update

--- egret/src/AddTodoForm.js
+++ egret/src/AddTodoForm.js
@@ -1,23 +1,19 @@
-import React from 'react';
-function AddTodoForm(props){
-const handleAddTodo = (event) => {
+import React from "react";
+function AddTodoForm(props) {
+  const handleAddTodo = (event) => {
     event.preventDefault();
     const todoTitle = event.target.title.value;
     console.log(todoTitle);
     props.onAddTodo(todoTitle);
     event.target.reset();
-    
- }
-    return(
-        <form onSubmit={handleAddTodo}>
-            <label htmlFor="todoTitle">Title</label>
-                
-                <input type="text" id="todoTitle" name="title"/>
-                <button type="submit">Add</button>
-                
-            
-        </form>
+  };
+  return (
+    <form onSubmit={handleAddTodo}>
+      <label htmlFor="todoTitle">Title</label>
 
-    );
+      <input type="text" id="todoTitle" name="title" />
+      <button type="submit">Add</button>
+    </form>
+  );
 }
 export default AddTodoForm;
--- egret/src/TodoListItem.js
+++ egret/src/TodoListItem.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 let TodoListItem = (props) => {
-    return (
-      <li>
-        <span>{props.todo.id}</span>
-        <span>{props.todo.title}</span>
-      </li>  
-    )
-
+  return (
+    <li>
+      <span>{props.todo.id}</span>
+      <span>{props.todo.title}</span>
+    </li>
+  );
 };
 
 export default TodoListItem;
