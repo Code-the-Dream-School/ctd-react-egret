1a120543dbfe07473d4fc8124a5c8bb945464c04
Kenneth <kenldurham@gmail.com>
2021-09-14 16:21:55 -0700

    complete1-4

--- egret/src/TodoList.js
+++ egret/src/TodoList.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-const todolist = [
-    {
-      id: '1 ',
-      title: 'Read assigment'
-    },
-    {
-      id: '2 ',
-      title: 'Watch Videos'
-    },
-    {
-      id: '3 ',
-      title: 'Complete and submit assignment'
-    },
-    ];
-function TodoList (){
+
+function TodoList ({todoList}){
 return(
 <ul>
-   {todolist.map(function(item){
+   {todoList.map(function(item){
   return (
   <TodoListItem key={item.id} todo={item}/>
   );
