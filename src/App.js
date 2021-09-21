8e945d6131b4c6e8f2c138d8d93b9e36a485e335
Alena <83471057+alenamedved@users.noreply.github.com>
2021-09-20 21:37:04 -0500

    Side effects, custom hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
+//custom hook
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(
+    //retrieve the initial value from localstorage or pass an empty string
+    JSON.parse(localStorage.getItem("savedTodoList")) || []
+  );
+
+  useEffect(() => {
+    //side effect handler function to save the list in the localstorage
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+};
+
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
