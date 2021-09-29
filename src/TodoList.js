352ecdbba04eb88df9f2a9999d9a454a8bfdd206
Chung Kao <chungckao@gmail.com>
2021-09-29 11:07:54 +0800

    Complete lesson 1.6 assignment

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-const TodoList = ({todoList}) => (
+const TodoList = ({todoList, onRemoveTodo}) => (
   <ul>
     {todoList.map((item) => (
-      <TodoListItem key={item.id} item={item} />
+      <TodoListItem key={item.id} item={item} onRemoveTodo={onRemoveTodo} />
     ))}
   </ul>
 );
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-const TodoListItem = ({item}) => <li>{item.title}</li>;
+const TodoListItem = ({item, onRemoveTodo}) => (
+  <li>
+    <span>{item.title}</span>
+    &nbsp;
+    <button type='button' onClick={() => onRemoveTodo(item.id)}>
+      Remove
+    </button>
+  </li>
+);
 
 export default TodoListItem;
