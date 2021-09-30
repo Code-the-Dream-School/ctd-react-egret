b1e3034b1b02d9ee47d72b60de9142f563b07195
“kostik700015” <kostik700015@gmail.com>
2021-09-30 18:54:25 -0400

    lesson-1-5 done

--- application/src/App.js
+++ application/src/App.js
@@ -2,17 +2,37 @@
 import './App.css';
 import TodoList from './Todolist';
 import AddTodoForm from './AddTodoForm';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
+
+const useSemiPersistentState = () => {
+
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem('savedTodoList') || '[]')
+  );
+
+  useEffect(() => {
+    const json = JSON.stringify(todoList);
+    localStorage.setItem('savedTodoList', json )
+  },[todoList])
+
+  return [todoList, setTodoList]
+
+}
 
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+
+
+  const [todoList, setTodoList] = useSemiPersistentState()
+
+
     const addTodo = (newTodo) => {
       setTodoList([...todoList, newTodo])
     } 
 
   return (
+    <>
     <div className="Header">
       <header>
         <h1>Todo List</h1>
@@ -21,6 +41,7 @@ function App() {
         <p>Added Succesfully: {(todoList.length !==0)? todoList[todoList.length-1].title : null}</p>
         <TodoList todoList={todoList} />
     </div>
+    </>
   );
 }
 export default App;
