c5056ffd4a049dd10537da7ab69d4e525ed96448
tadesse123 <birbirsatadesse@gmail.com>
2021-10-03 15:38:36 -0700

    working on Reusable Components Component Composition Imperative React Inline Handler

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import React from 'react';
 import TodoList from "./TodoList";
 import AddTodoform from "./AddTodoForm";
 function App() {
+ 
   const[todoList, setTodoList] = React.useState([]);
 const addTodo = (newTodo) => {
   setTodoList ([...todoList, newTodo])
-}
+};
+function removeTodo(id) {
+  const newTodoList = todoList.filter(function(item){
+    return item.id !==id
+  })
+  setTodoList(newTodoList)
+};
  
   return (
     <div>
       <h1>Todo List</h1> 
       <AddTodoform onAddTodo={addTodo}/>
   
-      <TodoList todoList = {todoList}/>
+      <TodoList onRemoveTodo ={removeTodo} todoList = {todoList}/>
      
     </div>
   );
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react'
 
+function TodoListItem({todo,onRemoveTodo}) {
+    console.log({todo})
+    return (
+        <div>
+           <li key={todo.id}>{todo.title}<button  onClick={()=>onRemoveTodo(todo.id)} type="button">Remove</button> </li> 
 
-function TodoListItem({item}){
- 
- return (
-<li>{item.title}</li>
-);
+        </div>
+    )
 }
 
-export default TodoListItem;
+export default TodoListItem
