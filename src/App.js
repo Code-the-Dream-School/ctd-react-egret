abed90863d47baa03d26008335688f3f50a1cab6
Chung Kao <chungckao@gmail.com>
2021-10-12 15:46:24 +0800

    Implement useReducer hook as assignment 1.7 stretch goal

--- src/App.js
+++ src/App.js
@@ -3,50 +3,118 @@ import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 // import useSemiPersistentState from "./persistState";
 
+const getAsyncTodoList = () =>
+  new Promise((resolve, reject) =>
+    setTimeout(
+      () =>
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem("todoList")) || [],
+          },
+        }),
+      2000
+    )
+  );
+
+const [
+  todoListFetchInit,
+  todoListFetchSuccess,
+  todoListFetchFailure,
+  addTodoItem,
+  removeTodoItem,
+] = [
+  "TODOLIST_FETCH_INIT",
+  "TODOLIST_FETCH_SUCCESS",
+  "TODOLIST_FETCH_FAILURE",
+  "ADD_TODO_ITEM",
+  "REMOVE_TODO_ITEM",
+];
+
+const todoListReducer = (state, action) => {
+  switch (action.type) {
+    case todoListFetchInit:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case todoListFetchSuccess:
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case todoListFetchFailure:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case addTodoItem:
+      return {
+        ...state,
+        data: [...state.data, action.payload],
+      };
+    case removeTodoItem:
+      return {
+        ...state,
+        data: state.data.filter((todo) => todo.id !== action.payload),
+      };
+    default:
+      throw new Error();
+  }
+};
+
 const App = () => {
-  const [todoList, setTodoList] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
+  const [todoList, dispatchTodoList] = React.useReducer(todoListReducer, {
+    data: [],
+    isLoading: true,
+    isError: false,
+  });
 
   React.useEffect(() => {
-    new Promise((resolve, reject) => {
-      setTimeout(
-        () =>
-          resolve({
-            data: {
-              todoList: JSON.parse(localStorage.getItem("todoList")) || [],
-            },
-          }),
-        2000
-      );
-    }).then((result) => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    });
+    dispatchTodoList({type: todoListFetchInit});
+    getAsyncTodoList()
+      .then((result) => {
+        dispatchTodoList({
+          type: todoListFetchSuccess,
+          payload: result.data.todoList,
+        });
+      })
+      .catch(() => {
+        dispatchTodoList({type: todoListFetchFailure});
+      });
   }, []);
 
   React.useEffect(() => {
-    if (!isLoading) {
-      localStorage.setItem("todoList", JSON.stringify(todoList));
-    }
-  }, [todoList, isLoading]);
+    if (!todoList.isLoading)
+      localStorage.setItem("todoList", JSON.stringify(todoList.data));
+  }, [todoList]);
 
   const addTodo = (newTodo) => {
-    setTodoList((todoList) => [...todoList, newTodo]);
+    dispatchTodoList({
+      type: addTodoItem,
+      payload: newTodo,
+    });
   };
 
   const removeTodo = (id) => {
-    const newTodoList = todoList.filter((item) => item.id !== id);
-    setTodoList(newTodoList);
+    dispatchTodoList({
+      type: removeTodoItem,
+      payload: id,
+    });
   };
 
   return (
     <div>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      {isLoading ? (
+      {todoList.isError && <p>Something went wrong ...</p>}
+      {todoList.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+        <TodoList todoList={todoList.data} onRemoveTodo={removeTodo} />
       )}
     </div>
   );
--- src/persistState.js
+++ src/persistState.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
-    JSON.parse(localStorage.getItemd(key)) || initialState
+    JSON.parse(localStorage.getItem(key)) || initialState
   );
   React.useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
