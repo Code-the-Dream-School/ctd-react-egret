31f393ba86c85e0b4d5c363add82fbc59f5f68c6
tadesse123 <birbirsatadesse@gmail.com>
2021-10-20 15:28:00 -0700

    modified

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,21 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-
-function TodoList({todoList, onRemoveTodo}) {
+function TodoList({ todoList, onRemoveTodo }) {
   return (
-    <div>
+    <>
       <ul>
-        {todoList.map(function (item) {
-          return (<TodoListItem  key={item.id} todo={item} onRemoveTodo={onRemoveTodo}/>);
-        })}
+        {todoList.map((todo) => (
+
+          <TodoListItem
+            key={todo.id}
+            todo={todo}
+            onRemoveTodo={onRemoveTodo}
+          />
+
+        ))}
       </ul>
-    </div>
+    </>
   );
 }
 export default TodoList;
