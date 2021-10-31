516d4f3abd304ac06d90935d3356fc3649eac9e4
OmarAlareeki <omarproductions2018@gmail.com>
2021-10-31 09:22:53 -0700

    completed router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 import './style.css';
@@ -8,42 +9,28 @@ function App() {
   const [todoList, setTodoList] = useState([])
   const [isLoading, setIsLoading] = useState(true);
 
-  // useEffect(()=>{
-  //   new Promise((resolve, reject) => {
-  //       setTimeout(() => {
-  //         resolve({
-  //           data : { 
-  //             todolist : JSON.parse(localStorage.getItem('todoList')) 
-  //            } 
-  //          })
-  //       }, 2000);
-  //     }).then((result) => {
-  //       setTodoList(result.data.todolist);
-  //       setIsLoading(false)
-  //     })
-  //   }, [])
 
-    useEffect(() => {
-      fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`, 
-        {
-          method: 'GET', 
-            headers: {
-              Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-            },
-        })
+  useEffect(() => {
+    fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/Default`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+        },
+      })
       .then((resp) => resp.json())
       .then((data) => {
         setTodoList(data.records)
         setIsLoading(false)
       })
-    }, [])
+  }, [])
 
-    useEffect(() => {
-      if(isLoading === false) {
-       const json = JSON.stringify(todoList);
-       localStorage.setItem('todoList', json)
-      }
-     }, [todoList, isLoading]);  
+  useEffect(() => {
+    if (isLoading === false) {
+      const json = JSON.stringify(todoList);
+      localStorage.setItem('todoList', json)
+    }
+  }, [todoList, isLoading]);
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo])
@@ -57,13 +44,20 @@ function App() {
   }
 
   return (
-    <>
-      <header>
-        <h1>Todo List</h1>
-      </header>
-        <AddTodoForm onAddTodo={addTodo} />
-        {isLoading ? <span>Loading...</span> : <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>}
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact>
+          <header>
+            <h1>Todo List</h1>
+          </header>
+          <AddTodoForm onAddTodo={addTodo} />
+          {isLoading ? <span>Loading...</span> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
+        </Route>
+        <Route path='/new'>
+          <h1>New Todo List</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
