5382a27e126dbbaf80178569f107a223411d56cb
Ignat Babenko <ignatbabenko@Ignats-MacBook-Air.local>
2021-12-08 02:08:46 -0800

    lesson-5-1

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,12 +3,22 @@ import AddTodoForm from './AddTodoForm'
 import TodoList from './TodoList'
 import PropTypes from 'prop-types'
 
+const sortTodoItems = (objectA, objectB) => {
+  if (objectA.fields.Title < objectB.fields.Title) {
+    return -1
+  } else if (objectA.fields.Title === objectB.fields.Title) {
+    return 0
+  } else {
+    return 1
+  }
+}
+
 function TodoContainer({ tableName }) {
   const [todoList, setTodoList] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true)
 
   React.useEffect(() => {
-    fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${encodeURIComponent(tableName)}`,
+    fetch(`https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${encodeURIComponent(tableName)}?view=Grid%20view&sort[0][field]=Title&sort[0][direction]=asc`,
       {
         headers: {
           Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`
@@ -17,6 +27,7 @@ function TodoContainer({ tableName }) {
     )
       .then((resp) => resp.json())
       .then((data) => {
+        data.records.sort(sortTodoItems)
         setTodoList(data.records)
         setIsLoading(false)
       })
@@ -44,8 +55,11 @@ function TodoContainer({ tableName }) {
     )
       .then((resp) => resp.json())
       .then((data) => {
+        const addedItem = data.records[0];
+        const newTodoList = [...todoList, addedItem];
+        newTodoList.sort(sortTodoItems)
         console.log(data.records[0].id)
-        setTodoList([...todoList, ...data.records])
+        setTodoList(newTodoList)
       })
   }
 
